66d9743cecf546ed15a8aaed39a273a0253b745e
Jiri Marsik <jiri.marsik89@gmail.com>
2011-10-21 19:11:01 +0800

    Marshalling between text and blocks

---
+++ Scratchpad.hs
@@ -0,0 +1,34 @@
+import Char
+
+extractSubblocks :: Int -> Int -> Integer -> [Integer]
+extractSubblocks subblockSize subblockCount block =
+  let extractSubblocks' 0 block acc = acc
+      extractSubblocks' subblocksLeft block acc =
+        extractSubblocks' (subblocksLeft - 1) (block `div` 2^subblockSize)
+                          (block `mod` 2^subblockSize : acc)
+  in extractSubblocks' subblockCount block []
+
+mergeSubblocks :: Int -> Int -> [Integer] -> Integer
+mergeSubblocks subblockSize subblockCount subblocks =
+  let mergeSubblocks' 0 _ acc = acc
+      mergeSubblocks' subblocksLeft [] acc =
+        mergeSubblocks' (subblocksLeft - 1) [] (acc * 2^subblockSize)
+      mergeSubblocks' subblocksLeft (x:xs) acc =
+        mergeSubblocks' (subblocksLeft - 1) xs (acc * 2^subblockSize + x)
+   in mergeSubblocks' subblockCount subblocks 0
+
+textToBlocks :: Int -> String -> [Integer]
+textToBlocks _ [] = []
+textToBlocks bytesPerBlock text =
+  let textToBytes = (map (fromIntegral . Char.ord)) . (filter Char.isLatin1)
+      bytesToBlock = mergeSubblocks 1 bytesPerBlock
+      textToBlock = bytesToBlock . textToBytes
+  in textToBlock text : (textToBlocks bytesPerBlock $ drop bytesPerBlock text)
+
+blocksToText :: Int -> [Integer] -> String
+blocksToText _ [] = ""
+blocksToText bytesPerBlock (b:bs) =
+  let blockToBytes = extractSubblocks 1 bytesPerBlock
+      bytesToText = map (Char.chr . fromIntegral)
+      blockToText = bytesToText . blockToBytes
+  in blockToText b ++ blocksToText bytesPerBlock bs
