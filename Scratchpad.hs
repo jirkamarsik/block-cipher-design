139ba9d2033bb40c31ad8ba6b81d5816871ffc0c
Jiri Marsik <jiri.marsik89@gmail.com>
2011-10-22 17:06:22 +0800

    Changing the final cipher to 128-bit block

--- Scratchpad.hs
+++ Scratchpad.hs
@@ -10,32 +10,10 @@ md5schedule :: KeySchedule String Integer
 md5schedule seed =
   md5i (Str seed) : (md5schedule $ md5s (Str seed))
 
-stupidRounds = 15
-
-stupidAddition x y = (x + y) `mod` 2^128
-
-stupidFlip block = 
-  let [x1,x2,x3,x4,x5,x6,x7,x8] = extractSubblocks 16 8 block
-  in mergeSubblocks 16 8 [x2,x1,x4,x3,x6,x5,x8,x7]
-
-stupidRoundFunction :: KeyedFunction Integer
-stupidRoundFunction key = (stupidAddition key) . stupidFlip
-
-stupidCipher :: KeyedFunction String
-stupidCipher key =
-  stupidFlip . (iterateCipher md5schedule stupidRoundFunction stupidRounds key)
-
-stupidDecipher :: KeyedFunction String
-stupidDecipher key =
-  let keySchedule = \key -> reverse $ take stupidRounds $ md5schedule key
-      inverseKeySchedule = \key -> map (\x -> 2^128 - x) (keySchedule key)
-  in stupidFlip
-     . (iterateCipher inverseKeySchedule stupidRoundFunction stupidRounds key)
-
-
-feistelRounds = 16
-feistelBlockBytes = 8
-feistelBlockBits = 8 * feistelBlockBytes
+md5skipSchedule :: KeySchedule String Integer
+md5skipSchedule seed =
+  map (\x -> let skip = x `mod` 2^6 in (x `div` (2^skip)) `mod` 2^64)
+      (md5schedule seed)
 
 macroMA :: KeyedFunction Integer
 macroMA key input =
@@ -44,15 +22,21 @@ macroMA key input =
       outputPair = maStructure (feistelBlockBits `div` 4) keyPair inputPair
   in mergeDuo (feistelBlockBits `div` 2) outputPair
 
+
+feistelRounds = 16
+feistelBlockBytes = 16
+feistelBlockBits = 8 * feistelBlockBytes
+feistelFunction = macroMA
+
 simpleFeistelCipher =
-  feistelCipher md5schedule feistelRounds feistelBlockBits macroMA
+  feistelCipher md5schedule feistelRounds feistelBlockBits feistelFunction
 
 simpleFeistelDecipher =
   feistelCipher (\key -> reverse $ take feistelRounds $ md5schedule key)
-                feistelRounds feistelBlockBits macroMA
+                feistelRounds feistelBlockBits feistelFunction
+
 
 main = do
   text <- readFile "blake-poems.txt"
   let cipher = simpleFeistelCipher "The super secret key."
   measureDiffusion feistelBlockBytes cipher text
-  measureCorrelation feistelBlockBytes cipher text
