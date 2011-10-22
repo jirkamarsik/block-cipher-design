6eabdd14d42bd3dc86603725751d61b2d90259ed
Jiri Marsik <jiri.marsik89@gmail.com>
2011-10-22 14:04:02 +0800

    Stupid addition and stupid flipping cipher.

--- Scratchpad.hs
+++ Scratchpad.hs
@@ -10,21 +10,30 @@ md5schedule :: KeySchedule String Integer
 md5schedule seed =
   md5i (Str seed) : (md5schedule $ md5s (Str seed))
 
-simpleRoundFunction :: KeyedFunction Integer
-simpleRoundFunction key block =
+stupidRounds = 15
+
+stupidAddition x y = (x + y) `mod` 2^128
+
+stupidFlip block = 
   let [x1,x2,x3,x4,x5,x6,x7,x8] = extractSubblocks 16 8 block
-  block `xor` key
+  in mergeSubblocks 16 8 [x2,x1,x4,x3,x6,x5,x8,x7]
+
+simpleRoundFunction :: KeyedFunction Integer
+simpleRoundFunction key = (stupidAddition key) . stupidFlip
 
 stupidCipher :: KeyedFunction String
-stupidCipher = iterateCipher md5schedule xor 16
+stupidCipher key =
+  stupidFlip . (iterateCipher md5schedule simpleRoundFunction stupidRounds key)
 
 stupidDecipher :: KeyedFunction String
-stupidDecipher =
-  let keySchedule = \key -> reverse $ take 16 $ md5schedule key
-  in iterateCipher keySchedule xor 16
+stupidDecipher key =
+  let keySchedule = \key -> reverse $ take stupidRounds $ md5schedule key
+      inverseKeySchedule = \key -> map (\x -> 2^128 - x) (keySchedule key)
+  in stupidFlip
+     . (iterateCipher inverseKeySchedule simpleRoundFunction stupidRounds key)
 
 main = do
-  text <- readFile "shakespeare-macbeth.txt"
+  text <- readFile "blake-poems.txt"
   let cipher = stupidCipher "The secret key!"
   measureDiffusion 16 cipher text
   measureCorrelation 16 cipher text
